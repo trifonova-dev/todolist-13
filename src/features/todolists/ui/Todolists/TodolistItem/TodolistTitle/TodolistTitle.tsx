90faf2b2b5c68e009f1b732cc9dcfe9ac8ac84d3
Natalia Trifonova <trifonova.dev@gmail.com>
2026-08-27 17:24:32 +0700

    train

--- src/features/todolists/ui/Todolists/TodolistItem/TodolistTitle/TodolistTitle.tsx
+++ src/features/todolists/ui/Todolists/TodolistItem/TodolistTitle/TodolistTitle.tsx
@@ -2,7 +2,7 @@ import { EditableSpan } from "@/common/components/EditableSpan/EditableSpan"
 import { useAppDispatch } from "@/common/hooks"
 import {
   changeTodolistTitleTC,
-  deleteTodolistAC,
+  deleteTodolistTC,
   type DomainTodolist,
 } from "@/features/todolists/model/todolists-slice"
 import DeleteIcon from "@mui/icons-material/Delete"
@@ -19,7 +19,7 @@ export const TodolistTitle = ({ todolist }: Props) => {
   const dispatch = useAppDispatch()
 
   const deleteTodolist = () => {
-    dispatch(deleteTodolistAC({ id }))
+    dispatch(deleteTodolistTC({ id }))
   }
 
   const changeTodolistTitle = (title: string) => {
